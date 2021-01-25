5c6fc1538a601becb74e248d55ad65a41ed39bdd
KrisKurb <kris_ra@mail.ru>
2021-01-25 20:04:33 +0300

    2,3practica

---
+++ 2,3practic/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <Header></Header>
+    <Converter></Converter>
+    <Resume></Resume>
+  </div>
+</template>
+<script>
+import Header from "@/components/Header";
+import Converter from "@/components/Converter";
+import Resume from "@/components/Resume";
+export default {
+  name: 'App',
+  components:{
+    Resume,
+    Header,
+    Converter
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ 2,3practic/src/components/Header.vue
@@ -0,0 +1,37 @@
+<template>
+<b-navbar type="dark" class="head_nav_color">
+    <b-navbar-nav class="d-flex align-items-center">
+      <b-nav-item class="d-flex ">
+        <span class="nav_logo">WorkHunt</span>
+      </b-nav-item>
+      <b-nav-item>
+        <span class="nav_click">Конвертер</span>
+      </b-nav-item>
+      <b-nav-item>
+        <span class="nav_click">Резюме</span>
+      </b-nav-item>
+    </b-navbar-nav>
+</b-navbar>
+</template>
+
+<script>
+export default {
+name: "Header"
+
+}
+</script>
+
+<style scoped>
+.head_nav_color {
+  background: cadetblue;
+  height: 65px;
+}
+.nav_logo {
+ font-size: 45px;
+  font-style: italic;
+  color: beige;
+}
+.nav_click {
+  font-size: 18px;
+}
+</style>
